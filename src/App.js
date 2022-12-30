ded1f9a17806253c8b81d21a36b42524f088b88b
tallururevanth <tallururevanth@gmail.com>
2022-12-30 08:19:12 +0530

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react";
-import "./style.css";
-import SingleInput from './components/SingleInput'
-import Singleip from "./components/Singleip";
+import React from 'react';
+import './style.css';
+import SingleInput from './components/SingleInput';
+import Singleip from './components/Singleip';
 export default function App() {
   return (
     <div>
-     <SingleInput/>
-     <Singleip/>
+      <Singleip />
     </div>
   );
 }
